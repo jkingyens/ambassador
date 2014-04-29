41f6fd8544b2d6945b3be49f226df3b16b7fe210
Jeff Kingyens <jeff.kingyens@gmail.com>
2014-04-28 19:01:31 -0700

    switch to dns-native

--- index.js
+++ index.js
@@ -1,20 +1,48 @@
-var dns = require('dns');
+var dns = require('native-dns');
 var net = require('net');
 
 // listen for TCP connection
 var server = net.createServer(function(socket) {
 
-  // lookup host and port from service record
-  dns.resolveSrv(process.argv[2], function (err, res) {
-   
+  // request service lookup
+  var question = dns.Question({
+    name: process.argv[2],
+    type: 'SRV',
+  });
+
+  // make request to linked DNS server
+  var req = dns.Request({
+    question: question,
+    server: {
+      address: process.env.DNS_PORT_53_UDP_ADDR,
+      port: process.env.DNS_PORT_53_UDP_PORT,
+      type: process.env.DNS_PORT_53_UDP_PROTO
+    },
+    timeout: 1000,
+    cache: false
+  });
+
+
+  req.on('timeout', function () {
+    console.log('Timeout in making request');
+  });
+
+  req.on('message', function (err, res) {
+
+    var answer = res.answer;
+
     // kill connection if we cant find answer
-    if (err || !res.length) {
+    if (err || !answer.length) {
       socket.end();
       return;
     }
 
-    var host = res[0].name;
-    var port = res[0].port;
+    // ignore weights for now
+    var host = answer[0].target;
+    var port = answer[0].port;
+    var ttl = answer[0].ttl;
+    var priority = answer[0].priority;
+    var weight = answer[0].weight;
 
     // connect to remote side
     var client = net.connect({
@@ -26,6 +54,8 @@ var server = net.createServer(function(socket) {
     });
 
   });
+
+  req.send();
 
 });
 
